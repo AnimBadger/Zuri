be1832d65dfc23ea693002fdc103d2b558744eeb
AnimBadger <anim.ansah.stephen@gmail.com>
2023-09-08 15:39:10 +0000

    added process port

--- ProjectOne/src/index.js
+++ ProjectOne/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get('/api', (req, res) => {
   const slackName = req.query.slack_name;
