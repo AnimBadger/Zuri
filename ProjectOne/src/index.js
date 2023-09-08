750105d0118e9f348bd75eaa3faf61714875f9b0
AnimBadger <anim.ansah.stephen@gmail.com>
2023-09-08 15:37:15 +0000

    first files ready to deploy

---
+++ ProjectOne/src/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const app = express();
+const port = 5000;
+
+app.get('/api', (req, res) => {
+  const slackName = req.query.slack_name;
+  const track = req.query.track;
+
+  if (!slackName || !track) {
+    return res.status(400).json({ error: "'slack_name' and 'track' parameters are required" });
+  }
+
+  const currentDay = new Date().toLocaleDateString('en-US', { weekday: 'long' });
+
+  const utcTime = new Date().toUTCString();
+  const utcOffsetHours = new Date().getTimezoneOffset() / 60;
+
+  if (Math.abs(utcOffsetHours) > 2) {
+    return res.status(400).json({ error: 'UTC time is not within +/-2 hours' });
+  }
+
+  const fileGithubUrl = 'https://github.com/AnimBadger/Zuri/ProjectOne/src/index.js';
+  const sourceCodeGithubUrl = 'https://github.com/AnimBadger/Zuri';
+
+  const response = {
+    slack_name: slackName,
+    current_day: currentDay,
+    current_utc_time: utcTime,
+    track: track,
+    file_github_url: fileGithubUrl,
+    source_code_github_url: sourceCodeGithubUrl,
+    status_code: 200
+  };
+  res.json(response);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
